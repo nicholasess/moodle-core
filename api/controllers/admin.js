fd59755b5e936ef9cb6479150408cf254b0e5143
Nicholas Eduardo <nicholasess@gmail.com>
2014-07-08 02:28:25 -0300

    adicionado estrutura admin -> controller/routes/views

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -26,35 +26,44 @@ module.exports = function(app) {
 	teacher_new: function(req, res){
 		res.render('admin/teacher/new');	
 	},
+	teacher_profile: function(req, res){
+		res.render('admin/teacher/profile');	
+	},
 	teacher_edit: function(req, res){
 		res.render('admin/teacher/edit');	
 	}
-	// ,
+	,
 	// //====================================
 	// //|  STUDENT
 	// //====================================
-	// student: function(req, res){
-	// 	res.render('admin/student/index');
-	// },
-	// student_new: function(req, res){
-	// 	res.render('admin/student/new');	
-	// },
-	// student_edit: function(req, res){
-	// 	res.render('admin/student/edit');	
-	// }	
-	// ,
-	// //====================================
-	// //|  COURSE
-	// //====================================
-	// course: function(req, res){
-	// 	res.render('admin/course/index');
-	// },
-	// course_new: function(req, res){
-	// 	res.render('admin/course/new');	
-	// },
-	// course_edit: function(req, res){
-	// 	res.render('admin/course/edit');	
-	// }					
+	student: function(req, res){
+		res.render('admin/student/index');
+	},
+	student_new: function(req, res){
+		res.render('admin/student/new');	
+	},
+	student_profile: function(req, res){
+		res.render('admin/student/profile');	
+	},
+	student_edit: function(req, res){
+		res.render('admin/student/edit');	
+	}	
+	,
+	//====================================
+	//|  COURSE
+	//====================================
+	course: function(req, res){
+		res.render('admin/course/index');
+	},
+	course_new: function(req, res){
+		res.render('admin/course/new');	
+	},
+	course_profile: function(req, res){
+		res.render('admin/course/profile');	
+	},
+	course_edit: function(req, res){
+		res.render('admin/course/edit');	
+	}					
 }
 	
 	return AdminController;
